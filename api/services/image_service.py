b93478bbbd60b2c8cb075dcd67b040f4feccf6a5
pedro8180 <rodriguezpedro1709@gmail.com>
2025-02-27 14:59:33 -0600

    update image service

--- api/services/image_service.py
+++ api/services/image_service.py
@@ -13,6 +13,12 @@ class ImageGenerator:
 
     def generate_images(prompts: list):
         image_urls = []
+        sub_folder = (datetime.today().strftime('%Y-%m-%d %H:%M:%S')).replace(" ","-").replace(":","-")
+        images_path = os.path.join(images_folder, sub_folder)
+
+        if not os.path.exists(images_path):
+            os.makedirs(images_path) 
+
         for prompt in prompts:
             result = client.images.generate(
                     model = model,
@@ -22,14 +28,8 @@ class ImageGenerator:
 
             image_url = json.loads(result.model_dump_json())['data'][0]['url']
             image = requests.get(image_url).content
-
-        #sub_folder = str(datetime.now())
-        #images_path = os.path.join(images_folder, sub_folder)
-        #if not os.path.exists(images_path):
-        #    os.makedirs(images_path) 
-
             image_name = f"genai_img_{uuid4()}"
-            with open(os.path.join(images_folder, f"{image_name}.jpg"), 'wb') as handler:
+            with open(os.path.join(images_folder, images_path, f"{image_name}.jpg"), 'wb') as handler:
                 handler.write(image)
 
             image_urls.append(image_url)
