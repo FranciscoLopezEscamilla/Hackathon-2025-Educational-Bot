64c6de6cb96c8fb924b33e8a66405394265a3c5e
francisco.lopez <francisco.lopez@accenture.com>
2025-03-11 21:19:03 -0700

    added path aliases support

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, AliasOptions } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@chatbot": path.resolve(__dirname, "src/features/chatbot"),
+    } as AliasOptions,
+  },
 });
